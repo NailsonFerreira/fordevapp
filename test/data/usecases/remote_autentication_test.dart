1bc63f4a109c8dee9f88e164f78d242acc906cdb
NailsonFerreira <nailson_ferreira@hotmail.com>
2023-05-20 15:28:14 -0300

    ensure RemoteAuthentication calls HttpClient with correct body

--- test/data/usecases/remote_autentication_test.dart
+++ test/data/usecases/remote_autentication_test.dart
@@ -2,33 +2,50 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:fordev/domain/usecases/usecases.dart';
+
 class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
+  final HttpClient? httpClient;
+  final String? url;
 
   RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<void>? auth() async {
-    await httpClient.request(url: url, method: 'post');
+  Future<void>? auth(AuthenticationParams params) async {
+    final body = {'email': params.email, 'password':params.secret};
+    await httpClient?.request(url: url, method: 'post', body: body);
   }
 }
 
 abstract class HttpClient {
   Future<void>? request({
-    required String url,
-    required String method,
+    required String? url,
+    required String? method,
+    Map? body,
   });
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test("Should call HttpClient with curret URL", () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
-    await sut.auth();
+  RemoteAuthentication? sut;
+  HttpClientSpy? httpClient;
+  String? url;
 
-    verify(httpClient.request(url: url, method: "post"));
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
+  });
+
+  test("Should call HttpClient with curret URL", () async {
+
+    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
+    await sut?.auth(params);
+
+    verify(httpClient?.request(
+      url: url,
+      method: "post",
+      body: {'email': params.email, 'password': params.secret}
+    ));
   });
 }
